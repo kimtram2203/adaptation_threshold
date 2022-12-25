650839c354b8a508f4e577960fd27a1ba8beec3b
kimtram2203 <19t1021276@husc.edu.vn>
2022-12-25 14:02:18 +0700

    update

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import cv2 as cv
 import numpy as np
+
 from matplotlib import pyplot as plt
 
+imageData = ['anh1.jpg', 'anh2.jpg', 'anh3.png', 'anh4.jpg', 'anh5.png', 'anh6.jpg']
+images = []
+titles = ['Mean Thresholding', 'Gaussian Thresholding']
 
-def image_partitioning(fileName):
-    img = cv.imread('./assets/' + fileName, 0)
+
+def image_partitioning(file_name):
+    img = cv.imread('./assets/' + file_name, 0)
     img = cv.medianBlur(img, 5)
-    ret, th1 = cv.threshold(img, 127, 255, cv.THRESH_BINARY)
-    th2 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, \
-                               cv.THRESH_BINARY, 11, 2)
-    th3 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, \
-                               cv.THRESH_BINARY, 11, 2)
-    titles = ['Original Image', 'Global Thresholding (v = 127)',
-              'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
-    images = [img, th1, th2, th3]
-    for i in range(4):
-        plt.subplot(2, 2, i + 1), plt.imshow(images[i], 'gray')
-        plt.title(titles[i])
+    th2 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11, 2)
+    th3 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 2)
+    images.append(th2)
+    images.append(th3)
+
+
+def run_data():
+    for i in range(len(imageData)):
+        image_partitioning(imageData[i])
+    print(len(images))
+    for j in range(len(images)):
+        plt.subplot(len(images) // 2, 2, j + 1), plt.imshow(images[j], 'gray')
         plt.xticks([]), plt.yticks([])
-    plt.show()
 
-
-image_partitioning('anh3.jpg')
+                
+run_data()
